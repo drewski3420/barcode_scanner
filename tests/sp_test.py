1c556449b141ee8aaec06c77536249d36bc6feea
Drew (Apollo) <21123428+drewski3420@users.noreply.github.com>
2025-09-09 12:26:23 -0400

    test

--- tests/sp_test.py
+++ tests/sp_test.py
@@ -7,10 +7,18 @@ import adafruit_st7789
 spi = board.SPI()
 dc = digitalio.DigitalInOut(board.D24)
 reset = digitalio.DigitalInOut(board.D25)
-cs = digitalio.DigitalInOut(board.D8)
+#cs = digitalio.DigitalInOut(board.D8)
 
 # Initialize the display
-display = adafruit_st7789.ST7789(spi, cs=cs, dc=dc, reset=reset, width=240, height=320, rotation=90)
+display = adafruit_st7789.ST7789(
+    spi,
+    width=240,
+    height=320,
+    reset=reset,
+    dc=dc,
+    cs_pin=board.CE0,   # <- use cs_pin instead of cs
+    rotation=90
+)
 
 # Fill the screen with a color
 display.fill(0x00FF00)  # Green
