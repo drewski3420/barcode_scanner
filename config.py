59ded02e6747502a042964b70f57421683603987
drewski3420 <21123428+drewski3420@users.noreply.github.com>
2025-10-21 16:26:38 -0400

    Cleaned up config

--- config.py
+++ config.py
@@ -8,11 +8,13 @@ def _parse_bool(val: Any) -> bool:
     return val.strip().lower() in ("1", "true", "yes", "on")
   return bool(val)
 
-TIMEOUT_MINUTES = float("0.5")
+TIMEOUT_MINUTES = float("20")
+HOST_URL = "https://records.thejowers.com"
+HID_DEVICE="/dev/input/event0"
 DISPLAY_WIDTH = int("320")
 DISPLAY_HEIGHT = int("240")
 DISPLAY_ROTATION = int("2") # (0, 1=90, 2=180, 3=270)
-HOST_URL = "https://records.thejowers.com"
-USE_TFT = bool("true")
-HID_DEVICE="/dev/input/event0"
-TFT_BACKLIGHT_PIN = -1
+DC_PIN = 25
+RST_PIN = 24
+#TFT_BACKLIGHT_PIN = -1
+#USE_TFT = bool("true")
