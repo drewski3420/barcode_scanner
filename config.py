4afb2ec16a299029ec7246dbf574d08f542986fe
drewski3420 <21123428+drewski3420@users.noreply.github.com>
2026-04-25 17:26:59 -0400

    fix rotation

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ HOST_URL = "https://records.thejowers.com"
 HID_DEVICE="/dev/input/event0"
 DISPLAY_WIDTH = int("320")
 DISPLAY_HEIGHT = int("240")
-DISPLAY_ROTATION = int("2") # (0, 1=90, 2=180, 3=270)
+DISPLAY_ROTATION = int("0") # (0, 1=90, 2=180, 3=270)
 DC_PIN = 25
 RST_PIN = 24
 #TFT_BACKLIGHT_PIN = -1
